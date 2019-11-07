0a076e557a1611b81d49e7b6b6b011ba1e1f061a
Ankur Maurya <39486237+MauryaAnkurr@users.noreply.github.com>
2019-11-07 12:27:20 +0530

    Update RootUrl.py

--- RootUrl.py
+++ RootUrl.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+ 
+    
 @app.route('/ankur')
 def secondcode():
     return 'Hello, Ankur Maurya.Here is your first API... !'
